f44feda4c8beef4ac6eca06000b7e5c0b1cf8b25
acevallo <alexcevallos08@hotmail.com>
2014-10-22 16:42:01 -0500

    alex7
    asd

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -7,7 +7,9 @@ import java.util.*;
 
 import models.*;
 
-public class Application extends GlobalController {
+//para trabajar con login
+//public class Application extends GlobalController {
+public class Application extends Controller{
 
 
 	public static void index() {
@@ -15,5 +17,19 @@ public class Application extends GlobalController {
     		
         render();
     }
+	
+	public static void list(String search, Integer size, Integer page) {
+        List<Usuario> docen = null;
+        page = page != null ? page : 1;
+        if(search.trim().length() == 0) {
+            docen = Usuario.all().fetch(page, size);
+        } else {
+            search = search.toLowerCase();
+            docen = Usuario.find("lower(nombre) like ?1 OR lower(cedula) like ?2 ", "%"+search+"%", "%"+search+"%").fetch(page, size);
+        }
+        render(docen, search, size, page);
+        System.out.println("Si se llama a la funcion");
+    }
+	
 
 }
